e98882c6f13a77bd796d78d605eeae58bdfce540
MobasirSarkar <mobasirgithubonly@gmail.com>
2024-05-31 18:47:13 +0530

    changes

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,33 @@
 --
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+--
+-- "Tsuzat/NeoSolarized.nvim",
+-- style = "dark",
+-- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+-- priority = 1000, -- make sure to load this before all the other start plugins
+-- transparent = true,
+-- enable_italics = true,
+-- floats = "dark",
+-- opts = {
+--   styles = {
+--     sidebars = "transparent",
+--     comments = { italic = true },
+--     keywords = { italic = true },
+--     functions = { bold = true },
+--     variables = {},
+--     string = { italic = true },
+--     underline = true, -- true/false; for global underline
+--     undercurl = true,
+--   },
+--
+--   day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+--   hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+--   dim_inactive = false, -- dims inactive windows
+--   lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+-- },
+-- config = function()
+--   vim.cmd([[ colorscheme NeoSolarized ]])
+-- end,
+--
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,29 +1,18 @@
 return {
-  "Tsuzat/NeoSolarized.nvim",
-  style = "dark",
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  transparent = true,
-  enable_italics = true,
-  floats = "dark",
-  opts = {
-    styles = {
-      sidebars = "transparent",
-      comments = { italic = true },
-      keywords = { italic = true },
-      functions = { bold = true },
-      variables = {},
-      string = { italic = true },
-      underline = true, -- true/false; for global underline
-      undercurl = true,
-    },
-
-    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-    hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive = false, -- dims inactive windows
-    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-  },
+  "sainnhe/everforest",
+  lazy = false,
+  priority = 1000,
   config = function()
-    vim.cmd([[ colorscheme NeoSolarized ]])
+    local ui = vim.g
+    ui.everforest_enable_italic = 1
+    ui.everforest_transparent_background = 1
+    ui.everforest_better_performance = 1
+    ui.everforest_ui_contrast = "high"
+    ui.everforest_diagnostic_text_highlight = 1
+    ui.everforest_current_word = "underline"
+    ui.everforest_background = "hard"
+    ui.everforest_float_style = "dim"
+    ui.everforest_diagnostic_line_highlight = 1
+    vim.cmd.colorscheme("everforest")
   end,
 }
